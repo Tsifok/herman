066b723998eb01d97df578b55547dc112f58e2a1
Tsifok <hermanmatias03@gmail.com>
2022-09-27 12:05:16 -0300

    actualizo login

--- prueba/alta.php
+++ prueba/alta.php
@@ -1,39 +1,48 @@
-<?php 
-
-$direction = "localhost";
-$user = "root";
-$password = "";
-$name_bd = "escuela";
-
-
-$conn = mysqli_connect($direction,$user,$password,$name_bd);
-
-if(!$conn){
-    die(mysqli_connect_error());
-}
-
-$sql_verification = "SELECT name FROM materias WHERE name = '".$_POST['materia']."'";
-$sql_verf_result = mysqli_query($conn,$sql_verification);
-$raw = (mysqli_fetch_assoc($sql_verf_result));
-
-
-
-if(!empty($_POST)){
-    if($raw['name'] == $_POST['materia'])
-    {
-        ?>
-        <script>alert("el nombre ya esta en uso");</script>
-        <?php
-        header("Location: index.php");
+<?php
+// ve que el form no este vacio
+if(!empty($_POST['materia'])){
+    
+    //se conecta al la bd
+    $direction = "localhost";
+    $user = "root";
+    $password = "";
+    $name_bd = "escuela";
+    $conn = mysqli_connect($direction,$user,$password,$name_bd);
+    //verifica la conxion
+    if(!$conn){
+        die(mysqli_connect_error());
     }
-    else{
-        $sql_consulta = "INSERT INTO materias(name) VALUES ('".$_POST['materia']."')";
-        $sql_result = mysqli_query($conn,$sql_consulta);
+
+    //consulta para verificar que lo ingresado no este repetido
+    $sql_verification = "SELECT * FROM materias WHERE name = '".$_POST['materia']."'";
+    $sql_verf_ressult = mysqli_query($conn,$sql_verification);
+    $row = mysqli_fetch_assoc($sql_verf_ressult);
+    //verifica la consulta anterior 
+    if(!$sql_verf_ressult){
+        die('error en query'.mysqli_errno($conn));
+    }
+    //verifica que los valores no sean iguales, sino redirige al form
+    if($row['name'] == $_POST['materia']){
         
-        if(!$sql_result){
-            die(mysqli_error($conn));
+        header("location: formulario.php?err=1");
+    }
+    //si los datos no son repetidos se los ingresa a la tabla 
+    else{
+        $sql_insert = "INSERT INTO materias(name) VALUES ('".$_POST['materia']."')";
+        $sql_verf_insert = mysqli_query($conn,$sql_insert);
+
+        if(!$sql_verf_insert){
+            die('Problema query Insert'.mysqli_error($conn));
+            header("location: formulario.php?err=0");
         }
+
     }
 }
+else{
+    header("location: formulario.php?err=2");
+}
+
+
+
 
 ?>
--- prueba/formulario.php
+++ prueba/formulario.php
@@ -8,8 +8,31 @@
 </head>
 <body>
     <form action="alta.php" method="POST">
-        <input type="text" name="materia" placeholder="Nombre materia">
+        <input type="text" name="materia" placeholder="Nombre materia" pattern="[a-zA-z]">
         <input type="submit" value="submit">
     </form>
+
+    <?php 
+    //falta añadir caracteres especiales
+
+        if(isset($_GET['err'])){
+
+            switch ($_GET['err']){
+                case "1":
+                    echo "<script>alert('El nombre ingresado ya esta en uso')</script>";
+                break;
+                case "0":
+                    echo "<script>alert('El nombre ingresado ha sido exitosamente ingresado a la base de datos')</script>";
+                break;
+                case "2":
+                    echo "<script>alert('El campo debe ser rellenado')</script>";
+                break;
+            }
+                
+            
+        }
+    
+    ?>
+
 </body>
 </html>
