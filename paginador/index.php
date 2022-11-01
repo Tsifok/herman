5b81b2dd3c03694dde52aff016c58b9254d3d0c2
CanepaFelipe <luisfcanepa@gmail.com>
2022-11-01 17:42:55 -0300

    falta css

--- paginador/index.php
+++ paginador/index.php
@@ -19,25 +19,28 @@
         <?php require "php/show.php"; ?>
       </tbody>
     </table>
-    <nav aria-label="Page navigation example">
-      <ul class="pagination justify-content-end">
-        <li class="page-item <?php echo($pag == 1 ? 'disabled' : '');?>">
-          <a class="page-link" href="<?php echo ($pag == 1 ? '' : 'index.php?pag=' . ($pag - 1)); ?>">Previous</a>
-        </li>
-        <?php echo ($pag == 1 ? "" : '<li class="page-item"><a class="page-link" href="index.php?pag=1">First</a></li>');?>
-        
-        <li class="page-item"><a class="page-link" href="index.php?pag=<?php echo ($pag >= $total_pages-2 ? $num = $pag-4 : ($pag == 1 ? $num = $pag : ($pag <= 3 ? $num = 1 : $num = $pag-2))); ?>"><?php echo $num?></a></li>
-        <li class="page-item"><a class="page-link" href="index.php?pag=<?php echo ($pag >= $total_pages-2 ? $num = $pag-3 : ($pag == 1 ? $num = $pag+ 1 : ($pag == 2 ? $num = $pag : $num = $pag-1 ))); ?>"><?php echo $num?></a></li>
-        <li class="page-item"><a class="page-link" href="index.php?pag=<?php echo ($pag >= $total_pages-2 ? $num = $pag-2 : ($pag == 1 ? $num = $pag +2 : ($pag == 2 ? $num = $pag+ 1 : $num = $pag)));?>"><?php echo $num; ?></a></li>
-        <li class="page-item"><a class="page-link" href="index.php?pag=<?php echo ($pag >= $total_pages-2 ? $num = $pag-1 : ($pag == 1 ? $num = $pag+3 : ($pag == 2 ? $num = $pag + 2 : $num = $pag+1))); ?>"><?php echo $num; ?></a></li>
-        <li class="page-item"><a class="page-link" href="index.php?pag=<?php echo ($pag >= $total_pages-2 ? $num = $pag : ($pag == 1 ? $num = $pag+4 : ($pag == 2 ? $num = $pag + 3 : $num = $pag+2))); ?>"><?php echo $num; ?></a></li>
-        
-        <?php echo ($pag == $total_pages ? "" : '<li class="page-item"><a class="page-link" href="index.php?pag='.$total_pages.'">Last</a></li>');?>
-        <li class="page-item <?php echo($pag == $total_pages ? 'disabled' : '');?>">
-          <a class="page-link" href="<?php echo ($pag == $total_pages ? '' : 'index.php?pag=' . ($pag + 1)); ?>">Next</a>
-        </li>
-      </ul>
-    </nav>
+    <div class="">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-end">
+          <li class="page-item <?php echo($pag == 1 ? 'disabled' : '');?>">
+            <a class="page-link" href="<?php echo ($pag == 1 ? '' : 'index.php?pag=' . ($pag - 1)); ?>">Previous</a>
+          </li>
+          <?php echo ($pag == 1 ? "" : '<li class="page-item"><a class="page-link" href="index.php?pag=1">First</a></li>');?>
+          
+          <li class="page-item"><a class="page-link" href="index.php?pag=<?php echo ($pag >= $total_pages-2 ? $num = $pag-4 : ($pag == 1 ? $num = $pag : ($pag <= 3 ? $num = 1 : $num = $pag-2))); ?>"><?php echo $num?></a></li>
+          <li class="page-item"><a class="page-link" href="index.php?pag=<?php echo ($pag >= $total_pages-2 ? $num = $pag-3 : ($pag == 1 ? $num = $pag+ 1 : ($pag == 2 ? $num = $pag : $num = $pag-1 ))); ?>"><?php echo $num?></a></li>
+          <li class="page-item"><a class="page-link" href="index.php?pag=<?php echo ($pag >= $total_pages-2 ? $num = $pag-2 : ($pag == 1 ? $num = $pag +2 : ($pag == 2 ? $num = $pag+ 1 : $num = $pag)));?>"><?php echo $num; ?></a></li>
+          <li class="page-item"><a class="page-link" href="index.php?pag=<?php echo ($pag >= $total_pages-2 ? $num = $pag-1 : ($pag == 1 ? $num = $pag+3 : ($pag == 2 ? $num = $pag + 2 : $num = $pag+1))); ?>"><?php echo $num; ?></a></li>
+          <li class="page-item"><a class="page-link" href="index.php?pag=<?php echo ($pag >= $total_pages-2 ? $num = $pag : ($pag == 1 ? $num = $pag+4 : ($pag == 2 ? $num = $pag + 3 : $num = $pag+2))); ?>"><?php echo $num; ?></a></li>
+          
+          <?php echo ($pag == $total_pages ? "" : '<li class="page-item"><a class="page-link" href="index.php?pag='.$total_pages.'">Last</a></li>');?>
+          <li class="page-item <?php echo($pag == $total_pages ? 'disabled' : '');?>">
+            <a class="page-link" href="<?php echo ($pag == $total_pages ? '' : 'index.php?pag=' . ($pag + 1)); ?>">Next</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
