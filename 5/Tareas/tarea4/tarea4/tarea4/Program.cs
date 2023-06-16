9eff016b20075681f79c612d24a09c01f52c9b8e
GastonAmenta <gasty.amenta.et26@gmail.com>
2023-06-16 08:32:51 -0300

    empiezo con la tarea 4

--- 5/Tareas/tarea4/tarea4/tarea4/Program.cs
+++ 5/Tareas/tarea4/tarea4/tarea4/Program.cs
@@ -6,11 +6,60 @@ using System.Threading.Tasks;
 
 namespace tarea4
 {
+    /* El ejercicio ya tiene que tener una estructura predefinida para que el usuario ponga el numero de la sucursal que quiere ingresar los datos 
+     y ya pueda ingresarlo, osea tiene que estar el array de objeto con el numero de la sucursal y su nombre predefinidos
+     */
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+           /* int sucursales = 0;
+            bool resultado;
+            do
+            {
+                Console.WriteLine("Ingrese la cantidad de pentagonos");
+                resultado = int.TryParse(Console.ReadLine(), out sucursales);
+                if (!resultado) Console.WriteLine("Error, debe ingresar un numero");
+            } while (!resultado);
+           */
         }
     }
+public class sucursales
+    {
+        //constructor
+        public sucursales() 
+        { 
+        }
+        //atributos
+        public string nombreSucursal;
+        public int numSucursal;
+        public int montoVenta;
+        public int cantEmpleados;
+        public int cantClientes;
+        public string nombreEmpleado;
+        //metodos
+        public void calcPromVentas()
+        {
+
+        }
+        public void calcPromEmpleados()
+        {
+
+        }
+        public void calcVentaTotal()
+        {
+
+        }
+        public void ventaMax()
+        {
+
+        }
+        public void ventaMin()
+        {
+
+        }
+    
+    }
+
+
 }
