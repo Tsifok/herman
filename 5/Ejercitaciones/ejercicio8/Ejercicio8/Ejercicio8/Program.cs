3fa22314947140c68750a3c8d1d7f5c09450f7e5
Tsifok <hermanmatias03@gmail.com>
2023-06-30 11:32:18 -0300

    empiezo tarea

--- 5/Ejercitaciones/ejercicio8/Ejercicio8/Ejercicio8/Program.cs
+++ 5/Ejercitaciones/ejercicio8/Ejercicio8/Ejercicio8/Program.cs
@@ -4,13 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+
+    Desarrolle en un código que contenga una clase para crear un objeto y manejar información
+    de un curso de la escuela técnica 26 Confederación Suiza donde cargara:
+    Nombre del curso : (ejemplo: 5to 11)
+    Turno
+    Especialidad
+    Cantidad de alumnos
+    Edad de cada alumno
+    Promedio general de cada alumno
+ 
+*/
+
 namespace Ejercicio8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int año;
+            string a;
+            curso objCurso = new curso();
+            // ---Nombre---
+            // Año del curso
+            Console.Write("Ingrese año : ");
+            año = functions.FuncionIP(6,0,true);
+            a = ((año).ToString()+" ");
+            // Division del curso
+            Console.Write("Ingrese division : ");
+            año = functions.FuncionIP(15, 0, true);
+            a += ((año).ToString());
+            
+            objCurso.division = a;
+            // ---Turno---
+            Console.Write($"Ingrese a que turno pertenece el curso {a} : mañana, tarde o noche ? : ");
 
+            //functions.FuncionY();
+            //archivo.nombre_dela_funcion
         }
+    }
+
+    public class curso
+    {
+        //Constructor
+        public curso() { }
+
+        //Atributos
+        public string division;
+        public string turno;
+        public string especialidad;
+        public int alumnos;
+        public int[] edades;
     }
 }
---
+++ 5/Ejercitaciones/ejercicio8/Ejercicio8/Ejercicio8/functions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio8
+{
+    internal class functions
+    {
+        public static int FuncionIP(int max = 0, int min= 0, bool cero = false)
+        {
+            bool resultado;
+            int salida;
+            do
+            {
+                string entrada = Console.ReadLine();
+                resultado = int.TryParse(entrada, out salida);
+                if (!resultado)
+                {
+                    Console.Write("Ingrese un NUMERO entero : ");
+                }
+                else if(max != 0 && salida > max && resultado)
+                {
+                    Console.Write($"Ingrese un numero menor a {max}");
+                    if (min != 0 || cero)
+                    {
+                        Console.Write($" y mayor {min} : ");
+                    }
+                    else
+                    {
+                        Console.Write(" : ");
+                    }
+                    Console.WriteLine("mayor a ");
+                    resultado = false;
+                }
+                else if (min != 0 || cero)
+                {
+                    if (salida < min && resultado)
+                    {                                         
+                        Console.Write($"Ingrese un numero menor a {min}");
+                        if (max != 0)
+                        {
+                            Console.Write($" y mayor a {max} : ");
+                        }
+                        else
+                        {
+                            Console.Write(" : ");
+                        }
+                        resultado = false;
+                    }                    
+                }
+                
+            } while (!resultado);
+
+            return salida;
+        }
+    }
+}
