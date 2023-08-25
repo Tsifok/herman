1f332746012cd045b34b38f42ad7a46936fadd4a
Tsifok <106192467+Tsifok@users.noreply.github.com>
2023-08-25 11:21:40 -0300

    creado ej 12

---
+++ 5/Ejercitaciones/ejercicio12/ejercicio12/ejercicio12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        class hospital
+        {
+             
+        }
+    }
+}
