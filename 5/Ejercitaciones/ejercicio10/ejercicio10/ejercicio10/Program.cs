0b1c393149a3fde414103a6ec0ae203a817c71ee
Tsifok <106192467+Tsifok@users.noreply.github.com>
2023-08-25 09:29:14 -0300

    Ej 10 terminado

---
+++ 5/Ejercitaciones/ejercicio10/ejercicio10/ejercicio10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            factorial objFactorial = new factorial();
+            Console.Write("Ingrese un numero del cual sacar el factorial : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"El factorial de {n} es {objFactorial.calculoFactorial(n)}");
+        }
+
+        public class factorial
+        {
+            //Metodos
+            public int calculoFactorial(int n)
+            {   
+                if(n == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return n * calculoFactorial(n - 1);
+                }
+             
+
+                
+            }
+
+        }
+    }
+}
